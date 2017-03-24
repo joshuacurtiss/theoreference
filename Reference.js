e36a03b5fe835bf26fdc3bb57298417ea43938ad
Josh Curtiss <git@curtiss.me>
2017-03-24 16:05:25 -0500

    Improvements to Reference class to make it useful.
    - sanitizeChapter and sanitizePar methods to clean up user input to a standardized method. Typically, follow convention of cue names in video files, with the exceptions of paragraphs, in which case, just use numbers.
    
    - Add ability to receive cues for a given chapter so it can more intelligently ascertain paragraphs. Set up to receive them but doesn't do anything with them yet.
    
    Note: It handles ranges (1-3) as one item, even though it will clean up the items on either side of the range. A future enhancement will be to make the list of items if we have a cue list.

--- Reference.js
+++ Reference.js
@@ -1,36 +1,73 @@
+const REGEX=require("./RegEx");
+
 class Reference {
     constructor(publication,chapter,pars) {
         this.publication=publication;
         this.chapter=chapter;
         this.pars=pars;
+        this.cues=[];
         return this;
     }
     get publication() {return this._publication}
     set publication(publication) {if(publication) this._publication=publication}
     get chapter() {return this._chapter}
-    set chapter(chapter) {if(chapter) this._chapter=parseInt(chapter)}
+    set chapter(chapter) {if(chapter) this._chapter=parseInt(this.sanitizeChapter(chapter))}
     get pars() {return this._pars}
     set pars(pars) {
         this._pars=[];
         if( pars && Array.isArray(pars) ) this._pars=pars;
         else if( pars && typeof pars=="number" ) this._pars.push(pars);
         else if( pars && typeof pars=="string" ) {
-            // TODO: Enhace to support references to ART or Q or embedded scriptures
             for( var p of pars.split(",") ) {
                 p=p.trim();
-                // TODO: Handle hyphen for ranges, if numbers. 
-                // TODO: Store as ints for paragraphs, remove the "P" if they put it in.
-                // if( p.split("-").length==2 ) {
-                //     var start=p.split("-")[0];
-                //     var end=p.split("-")[1];
-                //     if( start<=end ) {
-                //         for( var i=start ; i<=end ; i++ ) this._pars.push(parseInt(i));
-                //     }
-                // }
-                // else 
-                this._pars.push(p);
+                // TODO: Handle hyphen for ranges, if has cues. 
+                if( p.split("-").length==2 ) {
+                    var start=this.sanitizePar(p.split("-")[0]);
+                    var end=this.sanitizePar(p.split("-")[1]);
+                    p=start+"-"+end;
+                    this._pars.push(p);
+                } else {
+                    this._pars.push(this.sanitizePar(p));
+                }
             }
         }
+    }
+    get cues() {return this._cues}
+    set cues(cues) {
+        // Can receive an array of strings for cue names. But it also will receive an array of cues as the
+        // webvtt library would return, where each array item is an object and the name is in a "content" property. 
+        var sanitizedCues=[];
+        if( Array.isArray(cues) ) {
+            for( var c of cues ) {
+                if( typeof c=="object" && c.hasOwnProperty("content") ) sanitizedCues.push(c.content);
+                else if( typeof c=="string" ) sanitizedCues.push(c);
+            }
+        }
+        this._cues=sanitizedCues;
+        this.pars=this.pars; // Trigger a reparsing now that we have the cues.
+    }
+    sanitizeChapter(txt) {
+        return txt.replace(/^(?:ch|chapter)?\s*(\d+)$/i,"$1");
+    }
+    sanitizePar(txt) {
+        var parRE=new RegExp("^(?:par|p)?\\s*(CUENUMBER)$"
+                    .replace("CUENUMBER",REGEX.CUENUMBER_REGEX.source),"i");
+        var questionRE=new RegExp("^(?:question|q)\\s*(CUENUMBER)$"
+                    .replace("CUENUMBER",REGEX.CUENUMBER_REGEX.source),"i");
+        var cueRE=new RegExp("^(CUESWITHNUMBERS)\\s*(CUENUMBER)?$"
+                    .replace("CUESWITHNUMBERS",REGEX.CUESWITHNUMBERS_REGEX.source)
+                    .replace("CUENUMBER",REGEX.CUENUMBER_REGEX.source),"i");
+        if( parRE.test(txt) ) {
+            txt=txt.replace(parRE,"$1");
+        } else if( questionRE.test(txt) ) {
+            txt=txt.replace(questionRE,"Q $1");
+        } else if( cueRE.test(txt) ) {
+            var match=cueRE.exec(txt);
+            txt=match[1]+(match[2]?` ${match[2]}`:``);
+        }
+        // Capitalize
+        txt=txt[0].toUpperCase() + txt.slice(1);
+        return txt;
     }
     parsToString() {
         var i=0;
@@ -55,6 +92,9 @@ class Reference {
     }
     valid() {
         return this.publication && this.chapter;
+    }
+    hasCues() {
+        return (this.cues.length>0);
     }
     isAllPars() {
         return (this.pars.length==0);
