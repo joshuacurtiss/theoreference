e8fa53801f50211c22cbf6baf0ec5871a0219261
Josh Curtiss <git@curtiss.me>
2017-04-07 22:59:35 -0500

    Better handling of "Art" cues.

--- RegEx.js
+++ RegEx.js
@@ -2,7 +2,7 @@
 module.exports={
     REFERENCE_REGEX: /\b(\w+)((?:\s*(?:ch|chapter)?\s*(?:\d+)\s*[:\.]?\s*(?:(?:CUESWITHNUMBERS\s*CUENUMBER?|\d+|CUESMISC)(?:\s*[\-,]\s*(?:CUESWITHNUMBERS\s*CUENUMBER?|\d+|CUESMISC))*)*;*)*)\b/gi,
     CHAPTERCUE_REGEX: /(?:ch|chapter)?\s*(\d+)\s*[:\.]?\s*((?:CUESWITHNUMBERS\s*CUENUMBER?|\d+|CUESMISC)(?:\s*[\-,]\s*(?:CUESWITHNUMBERS\s*CUENUMBER?|\d+|CUESMISC))*)*/gi,
-    CUESWITHNUMBERS_REGEX: /(?:art caption|art \d caption|presentation intro|article|service meeting|subheading|verse|summary|art|box \d+ p|box|par|question|review|presentation|p|q|r)/i,
+    CUESWITHNUMBERS_REGEX: /(?:art\s*\d?\s*(?:caption|start|end)|presentation intro|article|service meeting|subheading|verse|summary|art|box \d+ p|box|par|question|review|presentation|p|q|r)/i,
     CUENUMBER_REGEX: /(?:\d{1,2}[a-f]?)/i,
     PARBOUNDARY_REGEX: /^(subheading|box|art|par|review|presentation|#\d|p\s|q\s|r\s)/i,
     CUESMISC_REGEX: /(start|title|introduction|chorus|publication title|title page|article questions|opening questions)/i
