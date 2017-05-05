671525ed359368a737d9f5e7c86bb2dd4dcd4f9c
Josh Curtiss <git@curtiss.me>
2017-05-05 17:27:47 -0500

    Added "build" as paragraph boundary.
    This enables cues like "Build Your Own" to trigger a stop.

--- RegEx.js
+++ RegEx.js
@@ -4,6 +4,6 @@ module.exports={
     CHAPTERCUE_REGEX: /(?:ch|chapter)?\s*(\d+)\s*[:\.]?\s*((?:CUESWITHNUMBERS\s*CUENUMBER?|\d+|CUESMISC)(?:\s*[\-,]\s*(?:CUESWITHNUMBERS\s*CUENUMBER?|\d+|CUESMISC))*)*/gi,
     CUESWITHNUMBERS_REGEX: /(?:art\s*\d?\s*(?:caption|start|end)|presentation intro|article|service meeting|subheading|verse|summary|art|box \d+ p|box|par|question|review|presentation|p|q|r)/i,
     CUENUMBER_REGEX: /(?:\d{1,2}[a-f]?)/i,
-    PARBOUNDARY_REGEX: /^(subheading|box|art|par|review|presentation|#\d|p\s|q\s|r\s)/i,
+    PARBOUNDARY_REGEX: /^(subheading|box|art|par|review|presentation|build|#\d|p\s|q\s|r\s)/i,
     CUESMISC_REGEX: /(start|title|introduction|chorus|publication title|title page|article questions|opening questions)/i
 };
